b1ca7bcde7ba57fe89a0bf3799d91c75197e29e9
Deekshith <deekshithpatel10@gmail.com>
2024-07-06 20:42:20 +0530

    Test firebase calls using Pages

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,7 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 console.log("Hello")
 
