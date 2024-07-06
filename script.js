19dd4387de85ea6b0536738da9ab39e6dff939f6
Deekshith <deekshithpatel10@gmail.com>
2024-07-06 15:10:37 +0530

    Code the login page and add Firebase

---
+++ script.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJ0PiBT41ebpjkfvcbMUU9ivd0PGn2IEE",
+  authDomain: "quiz-webapp-247fd.firebaseapp.com",
+  projectId: "quiz-webapp-247fd",
+  storageBucket: "quiz-webapp-247fd.appspot.com",
+  messagingSenderId: "817263869607",
+  appId: "1:817263869607:web:0e7ab1ef985017f7c92582",
+  measurementId: "G-82CMDT3L8G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
