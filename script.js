a09202d1118fa97e4d930d46b9b8dfe2b8fc1655
Deekshith <deekshithpatel10@gmail.com>
2024-07-06 20:35:31 +0530

    Test firebase calls using Pages

--- script.js
+++ script.js
@@ -19,3 +19,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+console.log("Hello")
+
+if( analytics ) {
+  console.log(app)
+}
