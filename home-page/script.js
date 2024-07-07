36a8f9d489267fd797bef9ab2e87a3e3b57d22b3
Deekshith <deekshithpatel10@gmail.com>
2024-07-07 23:53:54 +0530

    Code backend for home page

--- home-page/script.js
+++ home-page/script.js
@@ -1,9 +1,28 @@
-
-
 const logOutBtn = document.querySelector(".logOut-btn")
 
 logOutBtn.addEventListener("click", () => {
    sessionStorage.clear()
 
    window.open("../index.html", "_self")
+})
+
+const createQuizDiv = document.querySelector(".create-div")
+const attendQuizDiv = document.querySelector(".attend-div") 
+const responsesDiv = document.querySelector(".responses-div")
+const resultsDiv = document.querySelector(".results-div")
+
+attendQuizDiv.addEventListener("click", () => {
+   console.log("attend a quiz")
+})
+
+createQuizDiv.addEventListener("click", () => {
+   window.open("../create-quiz/create.html", "_self")
+})
+
+responsesDiv.addEventListener("click", () => {
+   console.log("see responses")
+})
+
+resultsDiv.addEventListener("click", () => {
+   console.log("check results")
 })
