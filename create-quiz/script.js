c76aaa6ae38af27fb32f9620b1bccdbeb3a0c80e
Deekshith <deekshithpatel10@gmail.com>
2024-07-09 14:09:08 +0530

    Finish js logic for create-quiz

--- create-quiz/script.js
+++ create-quiz/script.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 
-//redirecting to home and checking if login is done
+//home button logic, and load ID of user in header
 const homeBtn = document.querySelector(".home-btn")
 const IDelement = document.querySelector(".ID-el span")
 
@@ -35,6 +35,9 @@ homeBtn.addEventListener("click", () => {
 const saveQuestionBtn = document.querySelector(".save-btn")
 const deleteQuestionBtn = document.querySelector(".delete-btn")
 const saveQuizBtn = document.querySelector(".save-quiz-btn")
+const questionListDiv = document.querySelector(".questions-list")
+
+const allQuestionButtons = questionListDiv.children
 
 const questionInput = document.getElementById("question-input")
 const optionOneInput = document.getElementById("one-input")
@@ -43,11 +46,38 @@ const optionThreeInput = document.getElementById("three-input")
 const optionFourInput = document.getElementById("four-input")
 const correctOptionInput = document.querySelector(".correct-option")
 
-let numOfQuestions = 0
+const quizNameInput = document.getElementById("quiz-name-input")
+
+let numOfSavedQuestions = 0
+let currentQuestion = 1
 let questionsArray = []
+createNewEmptyQuestion()
+loadQuestion( currentQuestion )
 
 saveQuestionBtn.addEventListener("click", () => {
-   //validate all input 
+
+   if( validateInput() ) {
+      saveQuestionToArray()
+
+      if( currentQuestion === (numOfSavedQuestions + 1) ) {
+         numOfSavedQuestions++
+         currentQuestion++
+         createNewEmptyQuestion()
+         loadQuestion( currentQuestion )
+
+      } else {
+         currentQuestion = numOfSavedQuestions + 1
+         loadQuestion( currentQuestion )
+
+      }
+
+   } else {
+      showFieldEmpty()
+   }
+
+})
+
+function validateInput() {
    const question = questionInput.value 
    const optionOne = optionOneInput.value
    const optionTwo = optionTwoInput.value
@@ -55,39 +85,105 @@ saveQuestionBtn.addEventListener("click", () => {
    const optionFour = optionFourInput.value
    const correctOption = correctOptionInput.value
 
-   if( question && optionOne && optionTwo && optionThree && optionFour && correctOption ) {
-      questionsArray.push( [question, optionOne, optionTwo, optionThree, optionFour, correctOption] )
-      numOfQuestions += 1;
+   return ( question && optionOne && optionTwo && optionThree && optionFour && correctOption )
+}
 
-      questionInput.value = "" 
-      optionOneInput.value = ""
-      optionTwoInput.value = ""
-      optionThreeInput.value = ""
-      optionFourInput.value = ""
-      correctOptionInput.value = ""
+function saveQuestionToArray() {
 
-      console.log( questionsArray )
+   const question = questionInput.value 
+   const optionOne = optionOneInput.value
+   const optionTwo = optionTwoInput.value
+   const optionThree = optionThreeInput.value
+   const optionFour = optionFourInput.value
+   const correctOption = correctOptionInput.value
 
-      const questionListDiv = document.querySelector(".questions-list")
-      const newQuestionButton = document.createElement("button")
+   questionsArray[ currentQuestion-1 ] = [question, optionOne, optionTwo, optionThree, optionFour, correctOption]
+}
 
-      newQuestionButton.textContent = numOfQuestions
+function loadQuestion( questionNumber ) {
 
-      questionListDiv.appendChild( newQuestionButton )
-
-   } else {
-      showFieldEmpty()
+   for( let button of allQuestionButtons ) {
+      button.removeAttribute("id")
    }
+
+   questionInput.value = questionsArray[ questionNumber - 1 ] [0] 
+   optionOneInput.value = questionsArray[ questionNumber - 1 ] [1]
+   optionTwoInput.value = questionsArray[ questionNumber - 1 ] [2]
+   optionThreeInput.value = questionsArray[ questionNumber - 1 ] [3]
+   optionFourInput.value = questionsArray[ questionNumber - 1 ] [4]
+   correctOptionInput.value = questionsArray[ questionNumber - 1 ] [5]
+
+   currentQuestion = questionNumber
+
+   allQuestionButtons[ currentQuestion - 1 ].id = "selected-question"
+}
+
+function createNewEmptyQuestion()  {
+
+   questionInput.value = "" 
+   optionOneInput.value = ""
+   optionTwoInput.value = ""
+   optionThreeInput.value = ""
+   optionFourInput.value = ""
+   correctOptionInput.value = ""
+
+   const newQuestionButton = document.createElement("button")
+
+   newQuestionButton.textContent = numOfSavedQuestions + 1
+
+   saveQuestionToArray()
+
+   newQuestionButton.addEventListener("click", () => {
+      const questionNumber = Number( newQuestionButton.textContent )
+      loadQuestion( questionNumber )
+   })
+
+   questionListDiv.appendChild( newQuestionButton )
+}
+
+deleteQuestionBtn.addEventListener("click", () => {
+   if( currentQuestion <= numOfSavedQuestions ) {
+      removeQuestionFromArray()
+
+      for( let button in allQuestionButtons ) {
+         if( Number(button.textContent) >= currentQuestion ) {
+            button.textContent = ( Number( button.textContent ) - 1 )
+         }
+      }
+
+      questionListDiv.removeChild( questionListDiv.lastChild )
+      numOfSavedQuestions--
+      currentQuestion = numOfSavedQuestions + 1
+      loadQuestion( currentQuestion )
+   }
+
 })
+ 
+function removeQuestionFromArray() {
+   questionsArray.splice( currentQuestion-1, 1 )
+}
 
+//save quiz 
 saveQuizBtn.addEventListener("click", () => {
-   if ( numOfQuestions ) {
+   const quizName = quizNameInput.value
 
+   if ( numOfSavedQuestions && quizName ) {
+      questionsArray.pop()
+
+      setDoc(db, "quizzes", `${userName}_${quizName}`, {
+         questions: questionsArray
+      })
+
+      window.open("../home-page/home.html", "_self")
+
+   } else if ( numOfSavedQuestions ) {
+      showNoName()
    } else {
       showNoQuestions()
    }
 })
 
+//error messages functions
 function showFieldEmpty() {
    const errorMessageElement = document.querySelector(".message")
 
@@ -106,6 +202,18 @@ function showNoQuestions() {
    errorMessageElement.style.transition = 'none';
    errorMessageElement.style.opacity = '1';
    errorMessageElement.textContent = '*No question added.';
+   
+   errorMessageElement.offsetWidth
+   errorMessageElement.style.transition = 'opacity 3s';
+   errorMessageElement.style.opacity = '0';
+}
+
+function showNoName() {
+   const errorMessageElement = document.querySelector(".pallete-msg")
+
+   errorMessageElement.style.transition = 'none';
+   errorMessageElement.style.opacity = '1';
+   errorMessageElement.textContent = '*Please add a name.';
    
    errorMessageElement.offsetWidth
    errorMessageElement.style.transition = 'opacity 3s';
